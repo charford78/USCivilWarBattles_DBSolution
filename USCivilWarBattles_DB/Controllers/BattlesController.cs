e308d20956b737870ea984de3934921377340ff6
Charlie Ford <charles.ford1978@gmail.com>
2021-10-09 15:55:28 -0400

    Added GetbyPk, Create, Change and Remove methods in BattleController class.  Added code to main program to test GetbyPk and Create.

--- USCivilWarBattles_DB/Controllers/BattlesController.cs
+++ USCivilWarBattles_DB/Controllers/BattlesController.cs
@@ -20,9 +20,52 @@ namespace USCivilWarBattles_DB.Controllers
         {
             return _context.Battles.ToList();
         }
+        
         public Battle GetbyPk(int id)
         {
             return _context.Battles.Find(id);
+        }
+
+        public bool Create(Battle battle)
+        {
+            if(battle.Id != 0)
+            {
+                throw new Exception("You must enter 0 for Id!");
+            }
+            _context.Battles.Add(battle);
+            var rowsAffected = _context.SaveChanges(); 
+            if(rowsAffected != 1)
+            {
+                throw new Exception("Create failed!");
+            }
+            return true;
+        }
+
+        public bool Change(int Id, Battle battle)
+        {
+            if(Id != battle.Id)
+            {
+                throw new Exception("Id's do not match");
+            }
+            _context.Entry(battle).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            var rowsAffected = _context.SaveChanges();
+            if(rowsAffected != 1)
+            {
+                throw new Exception("Update failed!");
+            }
+            return true;
+        }
+
+        public bool Remove(int Id)
+        {
+            var battle = _context.Battles.Find(Id);
+            if(battle == null)
+            {
+                return false;
+            }
+            _context.Battles.Remove(battle);
+            _context.SaveChanges();
+            return true;
         }
     }
 }
--- USCivilWarBattles_DB/Program.cs
+++ USCivilWarBattles_DB/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using USCivilWarBattles_DB.Controllers;
+using USCivilWarBattles_DB.Models;
 
 namespace USCivilWarBattles_DB
 {
@@ -8,13 +9,33 @@ namespace USCivilWarBattles_DB
         static void Main(string[] args)
         {
             var batCtrl = new BattlesController();
-            var battles = batCtrl.GetAll();
+            
+            var richmt = new Battle()
+            {
+                Id = 0,
+                BattleCode = "RICHMT",
+                BattleName = "Battle of Rich Mountain",
+                Date = "July 11th, 1861",
+                Location = "Randolph County, VA (now WV)",
+                Theatre = "Eastern",
+                Victor = "USA"
+            };
+            batCtrl.Create(richmt);
 
-            foreach(var b in battles)
+            var battles = batCtrl.GetAll();
+            foreach (var b in battles)
             {
                 Console.WriteLine($"{b.Id} | {b.BattleCode} | {b.BattleName} " +
                     $"| {b.Date} | {b.Location} | {b.Theatre} | {b.Victor}");
             }
+
+            //var battle = batCtrl.GetbyPk(2);
+            //if(battle == null)
+            //{
+            //    Console.WriteLine("Not found!");
+            //}
+            //Console.WriteLine($"{battle.BattleName} | {battle.Date} | {battle.Location} " +
+            //                    $"| {battle.Theatre} | {battle.Victor}");
         }
     }
 }
